bd88fb4b186e53a96c488f975c9f677204ef9596
Ivan Postnikov <ivanpost@mail.ru>
2018-09-09 14:37:26 +0400

    Telnet test

--- Python27/Telnet.py
+++ Python27/Telnet.py
@@ -4,21 +4,12 @@ import os
 import telnetlib
 import sys
 
-hostname = '192.168.31.12'
-
-response = os.system("ping -c 10 192.168.31.12" )
-if response == 0:
-    print hostname, 'Ping OK'
-else:
-    print hostname, 'AQUARIUS not response!'
-    print response
-
 
 try:
     tn = telnetlib.Telnet("192.168.31.36", 23, 3)
 except:
     print 'Error telnet'
     sys.exit(0)
-
+tn.close()
 print 'Telnet OK'
 tn.close()
