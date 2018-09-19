2210eda01ace45f671c5cae94712a99eee728e49
Ivan Postnikov <ivanpost@mail.ru>
2018-09-19 17:08:39 +0400

    Newfunc

--- Python27/Aurllib27.py
+++ Python27/Aurllib27.py
@@ -3,14 +3,13 @@
 import urllib2
 import urllib
 import time
-import pycurl
-import StringIO
-import os.path
+#import StringIO
+#import os.path
 import pyping
 import sys
 
 hostname = '192.168.31.12' # Переменная ip адрес
-hexfname = 'Blink2.hex'  # Пременная имя файла
+hexfname = 'Blink1.hex'  # Пременная имя файла
 
 r = pyping.ping(hostname)
 
@@ -21,25 +20,13 @@ else:
     print "Sorry uC not response"
     sys.exit(0)
 
-# hexfile = urllib2.urlopen("https://hi-garden.ru/images/Flash/" + hexfname)
-# output = open(hexfname,'wb')
-# output.write(hexfile.read())
-# output.close()
+hexfile = urllib2.urlopen("https://hi-garden.ru/images/Flash/" + hexfname)
+
 
 url = 'http://' + hostname + '/pgm/sync'
 
-# Данные, которые хотим отправить
-query_args = { 'm':'10', 's':'', 'X':'' }
-
-# Производим urlencodes для словаря
-data = urllib.urlencode(query_args)
-
-# Отправляем HTTP POST запрос
-request = urllib2.Request(url, data)
-time.sleep(3)
-
 # Аргументы запроса, которые хотим отправить
-query_args = { 'm':'10', 's':'' }
+query_args = { 's':'' }
 
 # Производим urlencodes для query_args
 data = urllib.urlencode(query_args)
@@ -47,27 +34,16 @@ data = urllib.urlencode(query_args)
 # Отправляем HTTP POST запрос
 request = urllib2.Request(url, data)
 response = urllib2.urlopen(request)
-html = response.read()
 
-# Выводим результат
-print html
+time.sleep(2)
+
+request = urllib2.urlopen(url)
+print request.read()
 
 url = 'http://' + hostname + '/pgm/upload'
 
-c = pycurl.Curl()
-c.setopt(pycurl.URL, url)
-b = StringIO.StringIO()
-c.setopt(pycurl.WRITEFUNCTION, b.write)
-c.setopt(pycurl.POST, 1)
-filesize = os.path.getsize(hexfname)
-c.setopt(pycurl.POSTFIELDSIZE, filesize)
-fin = open(hexfname, 'rb')
-c.setopt(pycurl.READFUNCTION, fin.read)
-c.perform()
+request = urllib2.Request(url, hexfile.read())
+response = urllib2.urlopen(request)
 
-response_code = c.getinfo(pycurl.RESPONSE_CODE)
-response_data = b.getvalue()
-c.close()
-b.close()
-print response_code
-print response_data
+print response.read()
+
